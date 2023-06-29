660a45b5ad14a66605fe7e25f62951b5a457cc03
EnmCastle <EnmaCastle@outlook.es>
2023-06-29 15:13:40 -0300

    Change between sections

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 const sections = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorAll('.controls');
-const sectBtn = document.querySelectorAll('.control');
+const sectBtns = document.querySelectorAll('.controls .control');
 const mainContent = document.querySelector('.main-content');
 
 function PageTransitionEvent() {
   // button click active class
-  for (let i = 0; i < sectBtn.length; i++) {
-    sectBtn[i].addEventListener('click', function() {
-      let currentBtn = document.querySelector('.active-btn');
-      currentBtn.classList.toggle('active-btn');
-      this.classList.toggle('active-btn');      
-    });   
-  }
+  sectBtns.forEach((btn) => {
+    btn.addEventListener('click', function() {
+      const id = this.getAttribute('data-id');
+      
+      // Remove active class from all buttons
+      sectBtns.forEach((btn) => {
+        btn.classList.remove('active-btn');
+      });
+      
+      // Add active class to the clicked button
+      this.classList.add('active-btn');
+      
+      // Hide all sections
+      sections.forEach((section) => {
+        section.style.display = 'none';
+      });
+      
+      // Show the corresponding section
+      const targetSection = document.getElementById(id);
+      if (targetSection) {
+        targetSection.style.display = 'block';
+      }
+    });
+  });
 }
 
 // Call the function to initialize page transition event handling
